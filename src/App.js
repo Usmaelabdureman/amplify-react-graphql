4a4cf61f9240c01ae55d6b84c5a82ef3a077b3df
Usmaelabdureman <uabdureman@gmail.com>
2023-06-20 22:21:42 +0300

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1 style={{fontFamily:'sans-serif',color:'ButtonFace'}}>Follow me on</h1>
+          <ul style={{listStyleType:'none'}}>
+            <li> <a style={{textDecoration:'none'}} href='https://github.com/Usmaelabdureman' target='_blank' rel="nore noreferrer">Github</a></li>
+            <li> <a style={{textDecoration:'none'}} href='https://www.instagram.com/esmizth/'>Insta</a></li>
+          </ul>
+        </div>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
